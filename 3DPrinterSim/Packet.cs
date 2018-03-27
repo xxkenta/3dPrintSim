fa61651ed7ddf56da6b2a83c5964eb6e8939b8a4
Sarah <sarahwu@southern.edu>
2018-03-27 16:37:45 -0400

    Added initialization

--- 3DPrinterSim/Packet.cs
+++ 3DPrinterSim/Packet.cs
@@ -1,10 +1,24 @@
 ﻿using System;
-namespace _3DPrinterSim
+namespace PrinterSimulator
 {
     public class Packet
     {
-        public Packet()
+        public byte cmd;
+        public int Length;
+        public int checksum = 0;
+        public byte[] data;
+
+        public Packet(byte cmd, byte[] data)
         {
+            this.cmd = cmd;
+            this.data = data;
+            this.Length = data.Length;
+            findChecksum();
+        }
+
+        public void findChecksum()
+        {
+            
         }
     }
 }
