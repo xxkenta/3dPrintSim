8f4cfee8a119edc1e2e066ff19eda06ee6a827f5
Kevin Wright <kevinwright@southern.edu>
2018-03-28 22:13:22 -0400

    Added to Communication Protocol

--- 3DPrinterSim/CommunicationsProtocol.cs
+++ 3DPrinterSim/CommunicationsProtocol.cs
@@ -5,13 +5,27 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Hardware;
+using Firmware;
+using PrinterSimulator;
 
 namespace PrinterSimulator
 {
     public class CommunicationsProtocol
     {
+        PrinterControl control;
+        Packet printer;
+        byte[] buffer = new byte[32];
+
         public CommunicationsProtocol()
         {
+            int header1 = control.WriteSerialToHost(printer.getHeader(), printer.Length);
+            int header2 = control.ReadSerialFromHost(printer.getHeader(), printer.Length);
+
+            if (header1 != header2)
+            {
+                control.WriteSerialToFirmware(buffer, 0xFF);
+            }
+
         }
     }
 }
