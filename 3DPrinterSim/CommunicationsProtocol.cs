3f009bae8b6e0fe57e7eb5cf58b1195f6268b0ff
Sarah <sarahwu@southern.edu>
2018-04-01 16:49:17 -0400

    Made communication protocol more functional and added packet class to firmware

--- 3DPrinterSim/CommunicationsProtocol.cs
+++ 3DPrinterSim/CommunicationsProtocol.cs
@@ -1,64 +1,58 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 using Hardware;
-using Firmware;
-using PrinterSimulator;
 
 namespace PrinterSimulator
 {
     public class CommunicationsProtocol
     {
-        public void sendPacket(PrinterControl printer, Packet packet, FirmwareController firmware)
+        public void SendPacket(PrinterControl printer, Packet packet)
         {
-            byte[] header = packet.getHeader();
+            byte[] header = packet.GetHeader();
             printer.WriteSerialToFirmware(header, header.Length);
-            byte[] responseHeader = firmwareReadPacket(printer, header.Length);
+            byte[] responseHeader = ReadPacket(printer, header.Length);
 
-            if(sameHead(header, responseHeader) == true)
+            if(SameHead(header, responseHeader) == true)
             {
                 printer.WriteSerialToFirmware(new byte[] { 0xA5 }, 1);
                 printer.WriteSerialToFirmware(packet.data, packet.data.Length);
 
-                byte[] character = firmwareReadPacket(printer, 1);
+                byte[] character = ReadPacket(printer, 1);
                 string response = "";
                 while(character[0] != 0)
                 {
-                    response += ASCIIEncoding.ASCII.GetString(new byte[] { character[0] });
-                    character = firmwareReadPacket(printer, 1);
+                    response += Encoding.ASCII.GetString(new byte[] { character[0] });
+                    character = ReadPacket(printer, 1);
                 }
 
                 if(response == "SUCCESS")
                 {
                     return;
                 }
-                sendPacket(printer, packet, firmware);
+                if(response.Contains("VERSION"))
+                {
+                    return;
+                }
+
+                SendPacket(printer, packet);
             }
             printer.WriteSerialToFirmware(new byte[] { 0xFF }, 1);
-            sendPacket(printer, packet, firmware);
+            SendPacket(printer, packet);
             return;
         }
 
 
 
-        public byte[] firmwareReadPacket(PrinterControl printer, int expected) 
+
+        public byte[] ReadPacket(PrinterControl printer, int expected) 
         {
             byte[] data = new byte[expected];
-            printer.ReadSerialFromHost(data, expected);
+            printer.ReadSerialFromFirmware(data, expected);
             return data;
         }
 
 
-
-        public void processCmd(Packet packet, FirmwareController firmware)
-        {
-            
-        }
-
-        public bool sameHead(byte[] h1, byte[] h2)
+        public bool SameHead(byte[] h1, byte[] h2)
         {
             if(h1.Length != h2.Length)
             {
@@ -72,6 +66,16 @@ namespace PrinterSimulator
                 }
             }
             return true;
+        }
+
+
+
+        public enum Cmds
+        {
+            LASER = 0,
+            GALVOS = 1,
+            ZCOR = 2,
+            RESET = 3
         }
     }
 }
