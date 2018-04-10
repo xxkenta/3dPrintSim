1bdb4b0989f9c53ec28a09035499e4b22f6c69b4
Kevin Wright <kevinwright@southern.edu>
2018-04-10 15:53:04 -0400

    zRail

---
+++ 3DPrinterSim/ZRailController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Firmware;
+using Hardware;
+
+namespace ZRail
+{
+    public class ZRailController
+    {
+        public ZRailController()
+        {
+            
+        }
+
+        public int ConvertZ(int steps) 
+        {
+            return 0;
+        }
+
+        public void LimitVelocity()
+        {
+            
+        }
+
+        public void LimitAccelleration()
+        {
+            
+        }
+    }
+}
